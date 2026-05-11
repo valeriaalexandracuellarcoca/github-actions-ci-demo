a104adf7f520ce87e29f06d7d5587b967b9a7317
valeriaalexandracuellarcoca <valeriaalexandracuellarcoca@gmail.com>
2026-05-11 16:13:07 -0400

    ci: agrega paso de linting al pipeline

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
+  {
+    ignores: ["node_modules/"],
+  },
+];
